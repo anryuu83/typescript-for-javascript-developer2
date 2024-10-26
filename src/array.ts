417b949420960332e1d9aa2761f4d4f88b90feba
anryuu83 <anryuu83@gmail.com>
2024-10-26 21:20:54 +0900

    create arrya.ts

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let nums: number[] = [1, 2, 3];
+
+console.log(nums);
+
+let numbers: Array<number> = new Array(12, 24);
+
+console.log(numbers);
+
+let strs: Array<string> = ['tokyo', 'osaka'];
+console.log(strs);
+
+let arr: (number | boolean | string)[] = [1, false, 'a'];
+console.log(arr);
