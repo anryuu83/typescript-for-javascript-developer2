fce42debfb8082899e065e80ea095e1f0f442d96
anryuu83 <anryuu83@gmail.com>
2024-10-26 21:08:45 +0900

    premitive type

---
+++ src/boolean.ts
@@ -0,0 +1,9 @@
+export {};
+let name = 'typescript';
+let isFinish: boolean = true;
+console.log({ isFinish });
+isFinish = false;
+
+console.log({ isFinish });
+let isOver: boolean = false;
+console.log({ isOver });
---
+++ src/number-string.ts
@@ -0,0 +1,10 @@
+export {};
+let year: number = 1986;
+console.log({ year });
+let age: number = 16;
+console.log({ age });
+
+let username: string = 'anryu';
+let password: string = 'password123';
+console.log({ username });
+console.log({ password });
