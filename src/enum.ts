4f0bd84b13b29a4fab8a8a1a48da0dc36e2cf2a0
anryuu83 <anryuu83@gmail.com>
2024-10-27 12:29:50 +0900

    practise enum

---
+++ src/enum.ts
@@ -0,0 +1,25 @@
+export {};
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Months.January);
+
+enum Colors {
+  RED = '#FF0000',
+  GREEN = '#00FF00',
+  BLUE = '#0000FF',
+}
+
+console.log(Colors.RED);
