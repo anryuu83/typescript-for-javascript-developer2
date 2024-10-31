e28f7323729143c17437d4a4ddaa324babaea527
anryuu83 <anryuu83@gmail.com>
2024-10-31 13:55:53 +0900

    practise readonly

--- src/readonly.ts
+++ src/readonly.ts
@@ -13,3 +13,14 @@ let foo: Person = {
 
 // foo.age = 3;
 console.log(foo);
+
+type ReadonlyProfile<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+let person2: ReadonlyProfile<Profile> = {
+  name: 'kawamoo',
+  age: 23,
+};
+
+console.log(person2);
